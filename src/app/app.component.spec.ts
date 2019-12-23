c95f33db8e90c52988eb58d83e3feb7f98b4ed2b
vbazanm <victor_bazan_milla@optum.com>
2019-12-23 21:15:22 +0000

    Remove title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,11 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('vhbazanm');
   });
-
+/*
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('vhbazanm');
-  });
+  }); */
 });
