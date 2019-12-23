4c95d425b92c53b466e623d3d15e6646c28b9348
vbazanm <victor_bazan_milla@optum.com>
2019-12-23 15:22:36 +0000

    Add login and logout routes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,20 +3,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { LogoutComponent } from './logout/logout.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent }
+  {path: 'login', component: LoginComponent },
+  {path: 'logout', component: LogoutComponent }
 ];
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forRoot(routes)
   ],
   exports: [
-    RouterModule
+    RouterModule,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
